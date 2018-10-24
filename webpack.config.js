20d3e62b6dbede2a7136fa6a4f4a2fb44e85e68d
notFromHere <jakubwasiljew@gmail.com>
2018-10-24 13:20:29 +0200

    more, more, more!!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const extractPlugin = new ExtractTextPlugin({
+    filename: 'main.scss'
+});
+
 module.exports = {
     entry: './src/js/app.js',
     output: {
-        path: `${__dirname}/dist/js`,
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     watch: true,
@@ -9,6 +19,12 @@ module.exports = {
     devtool: 'source-map',
     module: {
         rules: [
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: [ 'css-loader', 'sass-loader' ]
+                })
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -22,7 +38,50 @@ module.exports = {
                         } ] ]
                     }
                 }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'image/',
+                            publicPath: 'image/'
+                        }
+                    }
+                ]
             }
+
+        ]
+    },
+    plugins: [
+        extractPlugin,
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin([
+            'dist'
+        ])
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    compress: true,
+                    ecma: 6,
+                    mangle: true
+                },
+                sourceMap: true
+            })
         ]
     }
 };
